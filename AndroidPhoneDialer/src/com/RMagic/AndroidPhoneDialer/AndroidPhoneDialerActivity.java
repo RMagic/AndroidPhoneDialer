bb9b5e6a17a1d8c0df7f1d2c0e2d483bdf9f3361
RMagic <sim2bird@gmail.com>
2012-06-10 22:04:30 +0900

    implement dialButton.

--- AndroidPhoneDialer/src/com/RMagic/AndroidPhoneDialer/AndroidPhoneDialerActivity.java
+++ AndroidPhoneDialer/src/com/RMagic/AndroidPhoneDialer/AndroidPhoneDialerActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.net.Uri;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class AndroidPhoneDialerActivity extends Activity {
     /** Called when the activity is first created. */
@@ -11,9 +14,15 @@ public class AndroidPhoneDialerActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        Intent dialIntent = new
-        		Intent(Intent.ACTION_DIAL,Uri.parse("tel:5551212"));
-        dialIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(dialIntent);
+        
+        final Button callButton = (Button) findViewById(R.id.callButton);
+        callButton.setOnClickListener(new Button.OnClickListener() {
+        	public void onClick(View v) {
+        		Intent callIntent = new
+        				Intent(Intent.ACTION_CALL,Uri.parse("tel:5551212"));
+        		callIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        		startActivity(callIntent);
+        	}
+        });
     }
 }
